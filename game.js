9008448ad035e7293e5414204f0e252080f18e67
aidaskyh14 <aidaskyh24@gmail.com>
2026-08-19 09:40:20 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -16800,5 +16800,5 @@ async function V181_campaignScoreLater(summary){
   }
   if(document.readyState==="loading")document.addEventListener("DOMContentLoaded",start,{once:true});
   else start();
-  window.YAINOO_BUILD="V234-MISS-ALPACA-NEW-SUPPORT-ANIMATION";
+  window.YAINOO_BUILD="V235-MISS-ALPACA-CENTER-ONLY";
 })();
